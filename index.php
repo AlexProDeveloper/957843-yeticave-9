598cd887535a3096f45adab0948ed05cd8af5a92
AlexProDeveloper <alex_1998_plekhanov@mail.ru>
2019-04-13 16:38:57 +0300

    Fix

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
-<?php
-$is_auth = rand(0, 1);
-
-$user_name = 'Саша'; // укажите здесь ваше имя
-
-?>
+<?php require "data.php"; ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -54,54 +49,7 @@ $user_name = 'Саша'; // укажите здесь ваше имя
             <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
             <ul class="promo__list">
                 <!--заполните этот список из массива категорий-->
-                <?php
-                    $categories = [
-                        "Доски и лыжи",
-                        "Крепления",
-                        "Ботинки",
-                        "Одежда",
-                        "Инструменты",
-                        "Разное"
-                    ];
-                    $goods = [
-                          $good1 = [
-                                  "name" => "2014 Rossignol District Snowboard",
-                                  "category" => $categories[0],
-                                  "price" => 10999,
-                                  "url" => "img/lot-1.jpg"
-                          ],
-                          $good2 = [
-                              "name" => "DC Ply Mens 2016/2017 Snowboard",
-                              "category" => $categories[0],
-                              "price" => 159999,
-                              "url" => "img/lot-2.jpg"
-                          ],
-                          $good3 = [
-                            "name" => "Крепления Union Contact Pro 2015 года размер L/XL",
-                            "category" => $categories[1],
-                            "price" => 8000,
-                            "url" => "img/lot-3.jpg"
-                          ],
-                          $good4 = [
-                            "name" => "Ботинки для сноуборда DC Mutiny Charocal",
-                            "category" => $categories[2],
-                            "price" => 10999,
-                            "url" => "img/lot-4.jpg"
-                          ],
-                          $good5 = [
-                            "name" => "Куртка для сноуборда DC Mutiny Charocal",
-                            "category" => $categories[3],
-                            "price" => 7500,
-                            "url" => "img/lot-5.jpg"
-                          ],
-                          $good5 = [
-                            "name" => "Маска Oakley Canopy",
-                            "category" => $categories[5],
-                            "price" => 5400,
-                            "url" => "img/lot-6.jpg"
-                          ]
-                    ];
-                ?>
+
                 <?php foreach ($categories  as $key => $val): ?>
                     <li class="promo__item promo__item--boards">
                         <a  class="promo__link" href="pages/all-lots.html"><?php print($val); ?></a>

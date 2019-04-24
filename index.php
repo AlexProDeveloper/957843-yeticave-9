4b46ac0db9cf203f7878715825294813792e2ad5
AlexProDeveloper <alex_1998_plekhanov@mail.ru>
2019-04-24 22:10:24 +0300

    myCommit

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
- <?php
+<?php
 require "data.php";
 require "helpers.php";
 require "functions.php";
-
+require 'schema.sql';
+require 'queries.sql';
 $content = include_template("index.php", ["goods" => $goods, "categories" => $categories]);
 $footer = include_template("footer.php", ["categories" => $categories]);
 print include_template("layout.php", ["title" => "Главная", "content" => $content, "footer" => $footer]);
---
+++ queries.sql
@@ -0,0 +1,43 @@
+INSERT INTO users
+(name, email, password, avatar, contacts) VALUES
+('Тигран Ержанович', 'tigran@gmail.com', '$2y$10$f6qxLh68BtUFsnCdtSrEoOjLFwYsvMfEsFi/E5f5SR1MvIHMxU8LO', '', 'my skype : test'),
+('Василий Иванов', 'vasily@ya.ru', '$2y$10$w3fGSauUhdhGg0Uj7Meg9OTgqu2bxRJpaQxSw4iLX9d3WpeFjspWC', '', 'my contacts'),
+('Петр Игнатов', 'petr@mail.ru', '$2y$10$0JGIpnmor.SH5847tWG58uwdwbnpjagL8NiYc8vsemOdCDZLZbN6G', '', 'my contacts');
+
+INSERT INTO categories
+(name, code) VALUES ("Доски и лыжи", 'boards'), ("Крепления", 'attachment'), ("Ботинки", 'boots'), ("Одежда", 'clothing'),
+("Инструменты", 'tools'), ("Разное", 'other');
+
+INSERT INTO lots
+(url, category_id, name, start_price, user_id) VALUES
+("img/lot-1.jpg", 1, "2014 Rossignol District Snowboard", 10999, 1),
+("img/lot-2.jpg", 1, "DC Ply Mens 2016/2017 Snowboard", 159999, 1),
+("img/lot-3.jpg", 2, "Крепления Union Contact Pro 2015 года размер L/XL", 8000, 1),
+("img/lot-4.jpg", 3, "Ботинки для сноуборда DC Mutiny Charocal", 10999, 1),
+("img/lot-5.jpg", 4, "Куртка для сноуборда DC Mutiny Charocal", 7500, 1),
+("img/lot-5.jpg", 6, "Маска Oakley Canopy", 5400, 1);
+
+INSERT INTO bets
+(bet_price, user_id, lot_id) VALUES
+(100.4, 1, 4),
+(4562, 2, 4);
+
+SELECT l.name, l.start_price, l.url, (SELECT MAX(bet_price) FROM bets WHERE lot_id=l.id) as price FROM lots  AS l
+LEFT JOIN categories AS c ON l.category_id=c.id
+WHERE l.ended_at < NOW()
+ORDER BY l.created_at ASC;
+
+SELECT * FROM lots LEFT JOIN categories AS c ON lots.category_id = c.id WHERE lots.id = 1;
+
+UPDATE lots SET name='2014 Rossignol District Snowboard.' WHERE id=1;
+
+SELECT * FROM bets WHERE lot_id = 4 ORDER BY created_at ASC;
+
+-- SELECT u.name, l.name FROM lots as l
+-- LEFT JOIN users as u ON l.user_id=u.id
+--
+--
+-- SELECT *, (SELECT name FROM users) as user_name FROM lots
+--
+-- SELECT * FROM categories;
+
